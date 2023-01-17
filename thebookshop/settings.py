25e420825f5d19b0af83702bc5f627eafe4016cb
Gabriel S <gabriel.siket@gmail.com>
2023-01-17 19:40:52 +0000

    created forms and views for user app

--- thebookshop/settings.py
+++ thebookshop/settings.py
@@ -11,9 +11,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['yourdomain.com', '127.0.0.1', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://.127.0.0.1:8000']
-
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
